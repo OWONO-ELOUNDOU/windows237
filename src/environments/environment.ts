cc50b46d2f33867a00b13cf5c6afa3c8132d2ce1
OWONO ELOUNDOU <OWONO ELOUNDOU>
2023-03-15 12:34:03 +0100

    minor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
-  firebase: {
-    projectId: 'windows-237',
-    appId: '1:749432239695:web:143c73bead105b620d5c3a',
-    databaseURL: 'https://windows-237-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'windows-237.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyCpXsBXNgViqQB-UOwpw_NWuzcXLR4B7ng',
-    authDomain: 'windows-237.firebaseapp.com',
-    messagingSenderId: '749432239695',
-  }
+    firebase: {
+      projectId: 'windows-237',
+      appId: '1:749432239695:web:143c73bead105b620d5c3a',
+      databaseURL: 'https://windows-237-default-rtdb.europe-west1.firebasedatabase.app',
+      storageBucket: 'windows-237.appspot.com',
+      locationId: 'europe-west',
+      apiKey: 'AIzaSyCpXsBXNgViqQB-UOwpw_NWuzcXLR4B7ng',
+      authDomain: 'windows-237.firebaseapp.com',
+      messagingSenderId: '749432239695',
+    }
 };  
